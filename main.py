5b8548580301f1e6e446d7a31312a67addfb76e5
Ignitor1400 <70995063+Ignitor1400@users.noreply.github.com>
2021-06-22 00:44:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image 
+
+
+#histogram of the Original Image
+print("Histogram Of Original Image")
+img = cv2.imread('img.jpg',0)
+
+hist,bins = np.histogram(img.flatten(),256,[0,256])
+
+cdf = hist.cumsum()
+cdf_normalized = cdf * hist.max()/ cdf.max()
+
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+
+
+#Histogram equalization
+
+img = cv2.imread('img.jpg',0)
+
+# create a CLAHE object (Arguments are optional).
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+cl1 = clahe.apply(img)
+
+cv2.imwrite('res2.jpg',cl1)
+
+
+#Displaying Output
+im = Image.open(r"res2.jpg")
+im.show()
+
+#histogram of the eqalized Image
+
+print("Histogram Of Equalized IMage Image")
+img = cv2.imread('res2.jpg',0)
+
+hist,bins = np.histogram(img.flatten(),256,[0,256])
+
+cdf = hist.cumsum()
+cdf_normalized = cdf * hist.max()/ cdf.max()
+
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
